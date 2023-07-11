f7ddc8498d31f104a0bfeee42302f4e6a3cf8b85
Luiscarlosvd <lc26cr7@gmail.com>
2023-07-11 13:00:57 -0400

    Add unspecified to species using a transaction and rollback

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = B'1';
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Add unspecified to species in using transaction and rollback */
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
