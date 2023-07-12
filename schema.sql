a3e25448f51c4eacba3a90a88b6ad63c9eac2fcd
Luiscarlosvd <lc26cr7@gmail.com>
2023-07-12 11:40:15 -0400

    Add owners and species tables to vet_clinic database

--- schema.sql
+++ schema.sql
@@ -12,3 +12,20 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+
+/* Owners Table */
+
+CREATE TABLE owners (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(100),
+	age INT,
+	PRIMARY KEY(id)
+);
+
+/* Species Table */
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
