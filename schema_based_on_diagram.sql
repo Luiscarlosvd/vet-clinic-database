430d8749d75c649ce31ca72aa590df50707566c4
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-07-18 18:37:03 +0300

    Create the database from the diagram and create [atirnys table

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE hospital_clinic;
+
+CREATE TABLE patients (
+  id INTEGER GENERATED ALWAYS AS IDENTITY ,
+  name VARCHAR(255) NOT NULL ,
+  date_of_birth DATE ,
+  PRIMARY KEY (id)
+);
