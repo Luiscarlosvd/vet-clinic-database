f2c045fdd96ce013b2a9d4f734fc1cbb675e336b
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-07-18 19:12:57 +0300

    Add indexes to medical_histories and medical_history_treatments 20

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -58,6 +58,6 @@ CREATE INDEX treatment_id ON invoice_items(treatment_id);
 
 CREATE INDEX patient_id ON medical_histories(patient_id);
 
-CREATE INDEX medical_history_id ON medical_history_treatments(medical_history_id);
+CREATE INDEX medical_hist_id ON medical_history_treatments(medical_history_id);
 
-CREATE INDEX treatment_id ON medical_history_treatments(treatment_id);
+CREATE INDEX treat_id ON medical_history_treatments(treatment_id);
